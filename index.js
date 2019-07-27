4007bd03c0d2235c92a5bbcc28a3e3253c5b5f1b
Fiona Showalter <fionarose.work@gmail.com>
2019-07-26 19:52:22 -0700

    Updated title of read me file
    Updated title of read me file

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var quotes = [
+{
+  quote: "Don't cry because it's over smile because it happened." 
+  , source: "Dr Seuss",
+  tags: ["Grit", "Determination", "Success"]
+},
+{
+  quote: "You miss 100% of the shots you don't take.",
+  source: "Wayne Gretzky",
+  tags: ["Just Do It", "Sport motivation"]
+},
+{
+  quote: "Fall down, get back up.",
+  source: "Japanese Proverb",
+  tags: ["Determination", "Grit"]
+},
+{
+  quote: "I have not failed. I've just found 10,000 ways that do not work.",
+  source: "Thomas A. Edison",
+  citation: "quotesfast.com",
+  tags: ["Perserverance", "Innovation"]
+},
+{
+  quote: "The only thing we have to fear is fear itself.",
+  source: "Franklin D. Roosevelt",
+  year: "1993", 
+  tags: ["Hope", "Politics"]
+}
+];
+console.log(quotes);
+
